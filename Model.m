467c0bc512433226f601a1848892513c5200d1a0
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2024-06-08 22:20:53 +1200

    refactor functionality

--- Model.m
+++ Model.m
@@ -11,32 +11,57 @@ classdef Model < handle
     
     events ( NotifyAccess = private )
         % Event broadcast when the data is changed.
-        Shimmer1Changed
-        Shimmer2Changed
+        ConnectedShimmer
+        DisconnectedShimmer
+        ConfiguredShimmer
 
         SessionStarted
+        SessionEnded
+
     end % events ( NotifyAccess = private )
     
     methods
 
-        function connectShimmer( shimmerNum, obj ) 
+        function connectShimmer( shimmerName, obj )
+            
+            shimmerIndex = getShimmerIndexByName(shimmerName);
         
-            obj.Shimmers(shimmerNum).connect;
+            obj.Shimmers(shimmerIndex).connect;
 
-            notify( obj, "ConnectedShimmer" + shimmerNum )
+            notify( obj, "ConnectedShimmer" )
 
         end % connectShimmer
 
-        function disconnectShimmer( shimmerNum, obj ) 
-        
-            obj.Shimmers(shimmerNum).disconnect;
+        function disconnectShimmer( shimmerName, obj ) 
 
-            notify( obj, "DisconnectedShimmer" + shimmerNum )
+            shimmerIndex = getShimmerIndexByName(shimmerName);
+        
+            obj.Shimmers(shimmerIndex).disconnect;
+
+            notify( obj, "DisconnectedShimmer" )
 
         end % disconnectShimmer
 
-        function configureShimmers( obj) 
-        end
+        function configureShimmer( shimmerName, obj ) 
+            % Define settings for shimmer
+
+            shimmerIndex = getShimmerIndexByName(shimmerName);
+            shimmer = obj.Shimmers(shimmerIndex);
+            
+            SensorMacros = SetEnabledSensorsMacrosClass;                           % assign user friendly macros for setenabledsensors
+    
+            shimmer.setsamplingrate(51.2);                                         % Set the shimmer sampling rate to 51.2Hz
+    	    shimmer.setinternalboard('9DOF');                                      % Set the shimmer internal daughter board to '9DOF'
+            shimmer.disableallsensors;                                             % disable all sensors
+            shimmer.setenabledsensors(SensorMacros.GYRO,1,SensorMacros.MAG,1,...   % Enable the gyroscope, magnetometer and accelerometer.
+            SensorMacros.ACCEL,1);                                                  
+            shimmer.setaccelrange(0);                                              % Set the accelerometer range to 0 (+/- 1.5g for Shimmer2/2r, +/- 2.0g for Shimmer3)
+            shimmer.setorientation3D(1);                                           % Enable orientation3D
+            shimmer.setgyroinusecalibration(1);                                    % Enable gyro in-use calibration
+
+            notify( obj, "ConfiguredShimmer" )
+
+        end % configureShimmer
 
         function startSession( obj ) 
         
@@ -52,10 +77,27 @@ classdef Model < handle
             obj.Shimmers(1).stop;
             obj.Shimmers(2).stop;
 
-            notify( obj, "SessionStarted" )
+            notify( obj, "SessionEnded" )
 
-        end % startSession
+        end % endSession
         
     end % methods
+
+    methods (Access = private)
+
+        function shimmerIndex = getShimmerIndexByName( shimmerName, obj )
+
+            if (strcmp(obj.Shimmers(1).name, shimmerName))
+                shimmerIndex = 1;
+            elseif (strcmp(obj.Shimmers(2).name, shimmerName))
+                shimmerIndex = 2;
+            else
+                shimmerIndex = -1;
+                warning("Unable to retrieve index of Shimmer by name.");
+            end
+
+        end % shimmerIndex
+
+    end % private methods
     
 end % classdef
